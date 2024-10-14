566e1877b5d57b9aa3d1daac309d87fd30e2b7cd
pradadimas534@gmail.com <pradanadimas534@gmail.com>
2024-10-14 11:31:37 +0700

    Menyelesaikan tugas	praktikum Flutter

--- lib/pages/detail_buku.dart
+++ lib/pages/detail_buku.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:template_project/items/item.dart';
 
 class DetailBuku extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    final ItemArgs = ModalRoute.of(context)! .settings.arguments as Item;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(ItemArgs.judul),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            ItemArgs.gambar,
+            height: 150,
+            width: 100,
+            fit: BoxFit.cover,
+          )
+        ],
+      ),
+    );
   }
   
 }
--- lib/pages/halaman_home.dart
+++ lib/pages/halaman_home.dart
@@ -6,10 +6,17 @@ class HalamanHome extends StatelessWidget {
   final List<Item> items = [
     Item(
       judul: 'Jurnal Terror Libur Sekolah',
-      deskirpsi: 'Teruntuk saudara-saudaraku di Jurnal Risa; Saat ini akhirnya datang, kesempatan untuk menceritakan kisah kita bersama.',
+      deskirpsi:
+          'Teruntuk saudara-saudaraku di Jurnal Risa; Saat ini akhirnya datang, kesempatan untuk menceritakan kisah kita bersama.',
       penulis: 'Risa Saraswati',
       gambar: 'assets/item/buku1.png',
-    )
+    ),
+    Item(
+        judul: 'Jurnal Ph.D. mama',
+        penulis: 'Kanti pertiwi dkk',
+        deskirpsi:
+            'This book presents the struggles and joys of Indonesian women as they work towards achieving their aim: a Ph.D. The stories are often heart-wrenching, but in the end the struggle is worthwhile. –Lyn Parker, Professor of Asian Studies,',
+        gambar: 'assets/item/buku2.png')
   ];
 
   @override
@@ -20,18 +27,19 @@ class HalamanHome extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Expanded(child: GridView.builder(
-              padding: EdgeInsets.all(8),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.7,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-              ),
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-            return ItemCard(item: items[index]);
-          }))
+          Expanded(
+              child: GridView.builder(
+                  padding: EdgeInsets.all(8),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.7,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    return ItemCard(item: items[index]);
+                  }))
         ],
       ),
     );
