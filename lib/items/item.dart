78f8d7ac3c86a484c72cb59f7ba54a97e6780ede
pradadimas534@gmail.com <pradanadimas534@gmail.com>
2024-10-14 11:06:52 +0700

    menyelesaikan praktikum flutter

---
+++ lib/items/item.dart
@@ -0,0 +1,14 @@
+class Item {
+  final String judul;
+  final String penulis;
+  final String deskirpsi;
+  final String gambar;
+
+  Item({
+    required this.judul,
+    required this.penulis,
+    required this.deskirpsi,
+    required this.gambar,
+  });
+
+}
---
+++ lib/pages/halaman_home.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:template_project/items/item.dart';
+
+class HalamanHome extends StatelessWidget {
+  final List<Item> items = [
+    Item(
+      judul: 'Jurnal Terror Libur Sekolah',
+      deskirpsi: 'Teruntuk saudara-saudaraku di Jurnal Risa; Saat ini akhirnya datang, kesempatan untuk menceritakan kisah kita bersama.',
+      penulis: 'Risa Saraswati',
+      gambar: 'assets/item/buku1.png',
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daftar buku'),
+      ),
+      body: Column(
+        children: [
+          Expanded(child: GridView.builder(
+              padding: EdgeInsets.all(8),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.7,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+            return ItemCard(item: items[index]);
+          }))
+        ],
+      ),
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  final Item item;
+
+  const ItemCard({Key? Key, required this.item}) : super(key: Key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        SizedBox(
+          height: 8,
+        ),
+        Text('judul : ${item.judul}'),
+        SizedBox(
+          height: 10,
+        ),
+        TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/item', arguments: item);
+            },
+            child: Text('Detail')),
+      ]),
+    );
+  }
+}
